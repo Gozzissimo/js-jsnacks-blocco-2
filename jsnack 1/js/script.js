5d7c9f988e7c52932d3390744fb026f4067b04d8
Gozzissimo <fantini.nico@gmail.com>
2021-11-30 10:45:58 +0100

    rand array positions

--- jsnack 1/js/script.js
+++ jsnack 1/js/script.js
@@ -1,1 +1,36 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+
+const firstNames = [
+    'Marco',
+    'Giovanni',
+    'Mariangelo',
+    'Luca',
+    'Graziello',
+    'Concetta',
+    'Vito',
+    'Barnaba',
+    'Carlo',
+    'Agamennone',
+    'Giangiacomo'
+]
+
+const lastNames = [
+    'Garibaldi',
+    'Garasau',
+    'Rossi',
+    'Berlusconi',
+    'Berlinguer',
+    'Kennedy',
+    'Catone',
+    'Baraldi',
+    'Barbarossa',
+    'Russo',
+    'Shevchenko'
+]
+
+let randName = Math.floor(Math.random() * (firstNames.length) + 1);
+let randSur = Math.floor(Math.random() * (lastNames.length) + 1);
+
+console.log(randName);
+console.log(randSur);
+
