89c73c99471d7883400dd2de0872a1804535f4f6
Gozzissimo <fantini.nico@gmail.com>
2021-11-30 11:39:13 +0100

    test for per random N push

--- jsnack 2/js/script.js
+++ jsnack 2/js/script.js
@@ -1,1 +1,13 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const numbers = []
+
+for (let i = 0; i < 10; i++) {
+    
+    numbers.push = Math.floor(Math.random() * 101);
+    console.log(numbers);
+}
+
+// if (numbers[i] != 0 ) {
+//     let sum = 
+// }
